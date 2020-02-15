8873171cc1668a98f6f39b735c998b445bfd5b24
Christian Fernandez <cdferdez@gmail.com>
2020-02-15 00:48:22 -0600

    keras model working

--- lab_1/part4/model.py
+++ lab_1/part4/model.py
@@ -12,6 +12,10 @@ class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
 train_images = train_images / 255.0
 test_images = test_images / 255.0
 
+# add additional channel to data
+train_images = np.expand_dims(train_images, axis=3)
+test_images = np.expand_dims(test_images, axis=3)
+
 """ plt.figure(figsize=(10,10))
 for i in range(25):
     plt.subplot(5,5,i+1)
@@ -24,57 +28,69 @@ plt.show() """
 
 # build model
 model = keras.Sequential([
-    keras.layers.Conv2D(5,(28,28), stride=1,activation='relu'),
-    keras.layers.MaxPool2D(stride=2),
-    keras.layers.Conv2D(3, stride=1),
-    keras.layers.MaxPool2D(stride=2),
-    keras.layers.Flatten(input_shape=(28,28)),
-    keras.layers.Dense(128, activation='relu'),
+    keras.layers.Conv2D(3, (5,5), strides=1, activation='relu', input_shape=(28,28,1)),
+    keras.layers.MaxPooling2D((2,2)),
+    keras.layers.Conv2D(3, (3,3), strides=1, padding='same'),
+    keras.layers.MaxPooling2D((2,2)),
+    keras.layers.Flatten(),
+    keras.layers.Dense(100, activation='relu'),
+    keras.layers.Dense(50, activation='relu'),
     keras.layers.Dense(10)
 ])
+
+# print model summary
+print("\n\n\n")
+model.summary()
+print("\n\n\n")
 
 # compile model
 model.compile(optimizer='adam',
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               metrics=['accuracy'])
 
+# train and save the model
 model.fit(train_images, train_labels, epochs=10)
+model.save('model_cdf2')
 
 test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
 print('\nTest accuracy:', test_acc)
 
 predictions = model.predict(test_images)
 
+# remove channel from image
+train_images = train_images[:,:,:,0]
+test_labels = test_images[:,:,:,0]
+
 def plot_image(i, predictions_array, true_label, img):
-  predictions_array, true_label, img = predictions_array, true_label[i], img[i]
-  plt.grid(False)
-  plt.xticks([])
-  plt.yticks([])
+    predictions_array, true_label, img = predictions_array, true_label[i], img[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
 
-  plt.imshow(img, cmap=plt.cm.binary)
+    plt.imshow(img, cmap=plt.cm.binary)
 
-  predicted_label = np.argmax(predictions_array)
-  if predicted_label == true_label:
-    color = 'blue'
-  else:
-    color = 'red'
+    predicted_label = np.argmax(predictions_array)
+    if predicted_label == true_label:
+        color = 'blue'
+    else:
+        color = 'red'
 
-  plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
-                                100*np.max(predictions_array),
-                                class_names[true_label]),
-                                color=color)
+    plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
+                100*np.max(predictions_array),
+                class_names[true_label]),
+                color=color)
 
 def plot_value_array(i, predictions_array, true_label):
-  predictions_array, true_label = predictions_array, true_label[i]
-  plt.grid(False)
-  plt.xticks(range(10))
-  plt.yticks([])
-  thisplot = plt.bar(range(10), predictions_array, color="#777777")
-  plt.ylim([0, 1])
-  predicted_label = np.argmax(predictions_array)
+    predictions_array, true_label = predictions_array, true_label[i]
+    plt.grid(False)
+    plt.xticks(range(10))
+    plt.yticks([])
+    thisplot = plt.bar(range(10), predictions_array, color="#777777")
+    plt.ylim([0, 1])
+    predicted_label = np.argmax(predictions_array)
 
-  thisplot[predicted_label].set_color('red')
-  thisplot[true_label].set_color('blue')
+    thisplot[predicted_label].set_color('red')
+    thisplot[true_label].set_color('blue')
 
 i = 0
 plt.figure(figsize=(6,3))
