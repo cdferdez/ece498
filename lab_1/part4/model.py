59b4eafd4cca62d517ae7f6ed501b0a1df08b4a5
Christian Fernandez <cdf2@illinois.edu>
2020-02-15 19:05:59 -0600

    added loss history

--- lab_1/part4/model.py
+++ lab_1/part4/model.py
@@ -2,6 +2,8 @@ import tensorflow as tf
 from tensorflow import keras
 import numpy as np 
 import matplotlib.pyplot as plt
+import os
+import json
 
 fashion_mnist = keras.datasets.fashion_mnist
 (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
@@ -45,59 +47,33 @@ print("\n\n\n")
 
 # compile model
 model.compile(optimizer='adam',
-              loss="sparse_categorical_crossentropy",
-              #loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-              metrics=['accuracy'])
+            loss="sparse_categorical_crossentropy",
+            #loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+            metrics=['accuracy'])
 
-# train and save the model
-model.fit(train_images, train_labels, epochs=7)
-model.save('model_cdf2')
-model.save('model_cdf2.h5')
+# check for existing model and loss data
+model_path = "./model_cdf2"
+history = "yuhhh"
+if os.path.exists(model_path):
+    model = keras.models.load_model(model_path)
+    
+    # load loss history
+    with open('loss.json', 'r') as fp:
+        history = json.load(fp)
+else:
+    # train and save the model
+    history = model.fit(train_images, train_labels, epochs=10)
+    model.save('model_cdf2')
+    
+    # save loss data
+    history.history['acc'] = str(history.history['loss'])
+
+    with open('loss.json', 'w') as fp:
+        json.dump(history.history, fp)
 
 test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
 print('\nTest accuracy:', test_acc)
 
-predictions = model.predict(test_images)
-
-# remove channel from image
-# train_images = train_images[:,:,:,0]
-# test_labels = test_images[:,:,:,0]
-
-""" def plot_image(i, predictions_array, true_label, img):
-    predictions_array, true_label, img = predictions_array, true_label[i], img[i]
-    plt.grid(False)
-    plt.xticks([])
-    plt.yticks([])
-
-    plt.imshow(img, cmap=plt.cm.binary)
-
-    predicted_label = np.argmax(predictions_array)
-    if predicted_label == true_label:
-        color = 'blue'
-    else:
-        color = 'red'
-
-    plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
-                100*np.max(predictions_array),
-                class_names[true_label]),
-                color=color)
-
-def plot_value_array(i, predictions_array, true_label):
-    predictions_array, true_label = predictions_array, true_label[i]
-    plt.grid(False)
-    plt.xticks(range(10))
-    plt.yticks([])
-    thisplot = plt.bar(range(10), predictions_array, color="#777777")
-    plt.ylim([0, 1])
-    predicted_label = np.argmax(predictions_array)
-
-    thisplot[predicted_label].set_color('red')
-    thisplot[true_label].set_color('blue') """
-
-""" i = 0
-plt.figure(figsize=(6,3))
-plt.subplot(1,2,1)
-plot_image(i, predictions[i], test_labels, test_images)
-plt.subplot(1,2,2)
-plot_value_array(i, predictions[i],  test_labels)
-plt.show() """
+# plot loss
+plt.plot(history['loss'])
+plt.show()
