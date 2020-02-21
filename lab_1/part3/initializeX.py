823247bb3ae2881902ceb3df8e7ee9856a04be62
Christian Fernandez <cdferdez@gmail.com>
2020-02-21 17:26:52 -0600

    removed long ass comments

--- lab_1/part3/initializeX.py
+++ lab_1/part3/initializeX.py
@@ -1,18 +1,7 @@
-""" 
-    initializeX.function : returns the tensorflow variable X initialized to random numbers.
-""" 
 import tensorflow as tf
 import numpy as np
 
 def function(shape=(1,1)):
-    """ 
-        Returns a tensorflow variable initialize to random numbers
-
-        Args:
-            shape (tuple): represents the dimensions of the variable
-
-    """
-
     return tf.Variable(
             tf.random.uniform(shape)
     )
--- lab_1/part3/plotFunction.py
+++ lab_1/part3/plotFunction.py
@@ -1,16 +1,7 @@
-""" 
-    plotFunction.function(lossValues)
-"""
 import numpy as np 
 import matplotlib.pyplot as plt
 
 def function(lossValues):
-    """ 
-        Plots the loss values vs training steps
-
-        Args:
-            lossValues (list): loss values from loss minimization
-    """
     # convert to array, shape was kinda weird as a list
     lossValues = np.array(lossValues)
     
